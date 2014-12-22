37703e2c39482d8e68eea4ce4ce35c2acc3338ab
Edwin <edwincv0@gmail.com>
2014-12-21 22:10:15 -0800

    updated logic

--- twit.js
+++ twit.js
@@ -11,24 +11,27 @@ var phrases = ['Shop at Nutritonik.com!',
     'Check out our partner site at http://bit.ly/YjuUY6',
     'Live Healthy- Nutritonik.com'];
 
+// Returns a Random Time
+function getRandomTime() {
+    return Math.floor(Math.random() * (3600000 - 2800000)) + 2800000;
+}
+
 function getRandom(max_num){
     return Math.floor(Math.random()*max_num);
 }
 
-//Test Case
-//T.post('statuses/update', { status: phrases[getRandom(phrases.length)] }, function(err, data, response) {
-//    console.log(data);
-//    console.log(err);
-//});
+// Run to start
+T.post('statuses/update', { status: phrases[getRandom(phrases.length)] }, function(err, data, response) {
+    console.log(data);
+    console.log(err);
+});
 
-
-setTimeout(function(){
+setInterval(function(){
     T.post('statuses/update', { status: phrases[getRandom(phrases.length)] }, function(err, data, response) {
         console.log(data);
         console.log(err);
-//    console.log(response);
     });
-}, 3600000);
+}, getRandomTime());
 
 
 
